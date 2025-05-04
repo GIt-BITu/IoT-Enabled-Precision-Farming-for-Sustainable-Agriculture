08dd585c128a953c107544392c6175019f64ad72
Imed bourbia <imedbourbia4@gmail.com>
2025-05-04 12:21:44 +0100

    Techmain (front+back)-end

--- frontend/src/pages/TechChiefDashboard.jsx
+++ frontend/src/pages/TechChiefDashboard.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 import ChiefMain from "@/component/ChiefMain.jsx";
 import {Link, BrowserRouter as Router, Routes, Navigate} from "react-router-dom";
+import SideBar from "@/component/Dashboards/SideBar.jsx";
+const sideLinks = [
+    {
+        title: "Dashboard",
+        icon: "fa-solid fa-house",
+        path: `/chiefTechnician`
+    },
+    {
+        title: "teams",
+        icon: "fa-solid fa-people-group",
+        path: `/chiefTechnician/teams`
+    },
+    {
+        title: "Pending Tasks",
+        icon: "fa-solid fa-list-check",
+        path: `/chiefTechnician/pendingTasks`
+    }
+]
 
 
 function TechChiefDashboard(){
     return(<>
+            <main>
+                <div className="bg-[#f5f5dc] h-screen flex gap-5 p-5">
+                    <SideBar sideLinks={sideLinks} activeColor="bg-blue-950 text-[#f5f5dc]"
+                             inactiveColor="text-blue-950" hover="hover:bg-blue-950 hover:text-[#f5f5dc]">
+                       <div></div>
+                    </SideBar>
+                    <ChiefMain/>
+                </div>
 
-            <ChiefMain />
-    </>
+            </main>
+
+        </>
     )
 }
 
