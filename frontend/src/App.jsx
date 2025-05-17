b6a91c9d5986bbc2103b70922b9f4d1ed1e4c0f7
anes <anes.abdelmalek@univ-constantine2.dz>
2025-05-17 14:00:22 +0100

    Fix the Routing issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,15 @@
 import React from "react";
-import TechChiefDashboard from "@/pages/ChiefTechnician/TechChiefDashboard.jsx";
-import Task  from "@/pages/ChiefTechnician/Task.jsx";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import AllPendingTask from "@/pages/ChiefTechnician/AllPendingTask.jsx";
-import UpdateStatus  from "@/pages/ChiefTechnician/UpdateStatus.jsx";
-import CompletedTasks from "@/pages/ChiefTechnician/CompletedTasks.jsx";
-import GetReport from "@/pages/ChiefTechnician/GetReport.jsx";
-import Team from "@/pages/ChiefTechnician/Team.jsx";
+import { Routes, Route } from "react-router-dom";
+import LandingHome from "./pages/LandingHome.jsx";
 
 function App() {
     return (
-        <>
-            <div>
-                <Router>
-                    <Routes>
-                        <Route path="/chiefTechnician" element={<TechChiefDashboard />} />
-                        <Route path="/task/:id" element={<Task />} />
-                        <Route path="/PendingTasks" element={<AllPendingTask />} />
-                        <Route path="/updateTask/:id" element={<UpdateStatus />} />
-                        <Route path="/Completed-Tasks" element={<CompletedTasks />} />
-                        <Route path="/report/:id" element={<GetReport />} />
-                        <Route path="/teams" element={<Team/>} />
-                    </Routes>
-                </Router>
-            </div>
-        </>
-    )
+        <div className="bg-white">
+            <Routes>
+                <Route path="/" element={<LandingHome />} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
